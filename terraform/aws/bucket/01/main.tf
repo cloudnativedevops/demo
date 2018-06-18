ce50e1e984be83971383e73f7515d23a955cd4cd
Justin Domingus <domingusj@gmail.com>
2018-06-18 07:33:31 -0700

    Rename

--- terraform/aws/bucket/01/main.tf
+++ terraform/aws/bucket/01/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {}
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "<YOUR_NAME>-cloudnativedevops-demo-bucket"
+  bucket = "foo-cloudnativedevops-demo-bucket"
   acl    = "private"
 
   tags {
