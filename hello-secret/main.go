43c050553f86fede205a032ad7b3b60a5867ea05
John Arundel <john@bitfieldconsulting.com>
2018-06-21 10:52:35 +0100

    Change demo port number to 8888

--- hello-secret/main.go
+++ hello-secret/main.go
@@ -12,5 +12,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
