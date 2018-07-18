dd2b6f4d78fc1cc7de33951f4460a246c8046730
John Arundel <john@bitfieldconsulting.com>
2018-07-18 16:51:26 +0100

    Add ConfigMap-to-file example

---
+++ hello-config-file/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+const cfgPath = "/demo-config.yaml"
+
+var config struct {
+	Greeting string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s, 世界\n", config.Greeting)
+}
+
+func main() {
+	cfgData, err := ioutil.ReadFile(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = yaml.Unmarshal(cfgData, &config); err != nil {
+		log.Fatalf("failed to parse config file %q: %v", cfgPath, err)
+	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
