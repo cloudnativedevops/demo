a2c5aa6ef682621bef622bc9c85d4ef11f484022
Justin Domingus <domingusj@gmail.com>
2021-12-20 19:56:11 -0800

    Add log line

--- hello/main.go
+++ hello/main.go
@@ -12,5 +12,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	fmt.Println("Running demo app. Press Ctrl+C to exit...")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
