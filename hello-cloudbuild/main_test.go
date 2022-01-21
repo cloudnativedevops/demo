1a87a4729ae050bf287dc0dd284da0bec5532a73
佐々木優 <yu_sasaki@en-japan.com>
2022-01-21 09:12:35 +0900

    mod hola

--- hello-cloudbuild/main_test.go
+++ hello-cloudbuild/main_test.go
@@ -17,7 +17,7 @@ func TestHandler(t *testing.T) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusOK)
 	}
-	expected := "Hello, 世界\n"
+	expected := "Hola, 世界\n"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v, want %v", rr.Body.String(), expected)
 	}
